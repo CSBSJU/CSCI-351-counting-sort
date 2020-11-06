b78467f783ccd1acfe0b9e3370ba7d06221c1f8d
Jeremy Iverson <jiverson002@csbsju.edu>
2020-11-06 10:14:18 -0600

    Adds parallelization

--- csort.c
+++ csort.c
@@ -1,11 +1,99 @@
 /* assert */
 #include <assert.h>
 
+/* OpenMP API */
+#include <omp.h>
+
 /* EXIT_SUCCESS, rand */
 #include <stdlib.h>
 
 /* strtol */
 #include <stdio.h>
+
+static inline int
+MKPWROF2(int v)
+{
+  v--;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  return v+1;
+}
+
+static void
+scan(unsigned const n, unsigned * const a)
+{
+  unsigned * b;
+
+  #pragma omp parallel
+  {
+    /* get number of openmp threads as a power of 2 */
+    int const size = MKPWROF2(omp_get_num_threads());
+    /* get thread rank */
+    int const rank = omp_get_thread_num();
+
+    if (0 == rank) {
+      /* allocate memory for partial sums */
+      b = calloc(size, sizeof(*b));
+      assert(b);
+    }
+
+    /* local up-sweep (summation) */
+    unsigned sum = 0;
+    #pragma omp for schedule(static)
+    for (unsigned i = 0; i < n; ++i)
+      sum += a[i];
+
+    /* record private copy of b */
+    unsigned * const c = b;
+
+    /* record local up-sweep */
+    c[rank] = sum;
+
+    /* global up-sweep (reduction) */
+    int off=1;
+    for (int d = size / 2; d > 0; d /= 2, off *= 2) {
+      #pragma omp barrier
+      if (rank < d) {
+        unsigned const ai = off * (2 * rank + 1) - 1;
+        unsigned const bi = off * (2 * rank + 2) - 1;
+        c[bi] += c[ai];
+      }
+    }
+
+    /* clear for global down-sweep */
+    if (0 == rank)
+      c[size - 1] = 0;
+
+    /* global down-sweep */
+    for (int d = 1; d < size; d *= 2) {
+      off /= 2;
+      #pragma omp barrier
+      if (rank < d) {
+        unsigned const ai  = off * (2 * rank + 1) - 1;
+        unsigned const bi  = off * (2 * rank + 2) - 1;
+        unsigned const t = c[ai];
+        c[ai] = c[bi];
+        c[bi] += t;
+      }
+    }
+    #pragma omp barrier
+
+    /* local scan using offset from global scan */
+    unsigned p=c[rank];
+    #pragma omp for schedule(static) nowait
+    for (unsigned i = 0; i < n; ++i) {
+      unsigned const t = a[i];
+      a[i] = p;
+      p += t;
+    }
+  }
+
+  /* release memory for partial sums */
+  free(b);
+}
 
 static int
 csort(unsigned const k,
@@ -13,26 +101,53 @@ csort(unsigned const k,
       unsigned const * const in,
       unsigned       * const out)
 {
-  unsigned * const count = calloc(k + 1, sizeof(*count));
+  unsigned t;
+
+# pragma omp parallel master
+  t = omp_get_num_threads();
+
+  unsigned * const count = calloc(t * k + 1, sizeof(*count));
   if (NULL == count) {
     return -1;
   }
 
+  double const ts1 = omp_get_wtime();
+# pragma omp parallel for
   for (unsigned i = 0; i < n; i++) {
-    count[in[i]]++;
+    count[in[i] * omp_get_num_threads() + omp_get_thread_num()]++;
   }
+  double const te1 = omp_get_wtime();
+  printf("timer1: %lf\n", te1 - ts1);
 
-  unsigned total = 0;
-  for (unsigned i = 0; i <= k; i++) {
+  double const ts2 = omp_get_wtime();
+#if 1
+ unsigned total = 0;
+  for (unsigned i = 0; i <= t * k; i++) {
     unsigned const counti = count[i];
     count[i] = total;
     total += counti;
   }
-
-  for (unsigned i = 0; i < n; i++) {
-    out[count[in[i]]] = in[i];
-    count[in[i]]++;
+#elif 0
+  scan(t * k, count);
+#else
+  unsigned total = 0;
+# pragma omp simd reduction (inscan, +:total)
+  for (unsigned i = 0; i <= t * k; i++) {
+    count[i] = total;
+#   pragma omp scan exclusive(total)
+    total += count[i];
   }
+#endif
+  double const te2 = omp_get_wtime();
+  printf("timer2: %lf\n", te2 - ts2);
+
+  double const ts3 = omp_get_wtime();
+# pragma omp parallel for
+  for (unsigned i = 0; i < n; i++) {
+    out[count[in[i] * omp_get_num_threads() + omp_get_thread_num()]++] = in[i];
+  }
+  double const te3 = omp_get_wtime();
+  printf("timer3: %lf\n", te3 - ts3);
 
   free(count);
 
